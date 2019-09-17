f86f395ca526ba97d8875a301d54c37d57016bd8
Gustavo Leite <gustavoleite.ti@gmail.com>
2019-09-17 17:17:37 -0300

    Add decorator `timeit` to python library

--- python/library.py
+++ python/library.py
@@ -1,4 +1,6 @@
 import functools
+import time
+import sys
 
 
 def memoize(fn):
@@ -11,5 +13,20 @@ def memoize(fn):
         if n not in fn.table:
             fn.table[n] = fn(n)
         return fn.table[n]
+
+    return decorator
+
+
+def timeit(fn):
+    """Decorator to measure execution time"""
+
+    @functools.wraps(fn)
+    def decorator(*args, **kwargs):
+        t0 = time.monotonic()
+        ret = fn(*args, **kwargs)
+        t1 = time.monotonic()
+        print(f"INFO: Function {fn.__name__!r} executed in {t1-t0:.3} seconds",
+              file=sys.stderr)
+        return ret
 
     return decorator
